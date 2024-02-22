853dbbf87b7b1d9b404057fb4b503ffb417b66a3
Atinadcodes <lamptey.danita@gmail.com>
2024-02-21 16:34:04 -0800

    update the style of the navbar

---
+++ src/Pages/Landing/index.jsx
@@ -0,0 +1,24 @@
+import Hero from "../../components/Hero";
+import Navbar from "../../components/navbar/Navbar";
+import Products from "../../components/Products";
+import Signbutton from "../../components/Signbutton";
+
+
+const LandingPage = () => {
+  return (
+    <div>
+    <Navbar></Navbar>
+    <Hero></Hero>
+    <Products></Products>
+    <Signbutton></Signbutton>
+    
+
+    
+
+    </div>
+  )
+   
+  
+};
+
+export default LandingPage;
---
+++ src/components/Signbutton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Signbutton = () => {
+  return (
+    <div>Signbutton</div>
+  )
+}
+
+export default Signbutton
